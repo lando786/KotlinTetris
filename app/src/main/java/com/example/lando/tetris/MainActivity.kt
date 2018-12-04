43ddbd2cdbd80dc507cd97483bd50bf70f4f5a40
Luis Dasta <ldasta786@gmail.com>
2018-12-03 19:03:23 -0600

    GameActivity with Navigation
    Added game activity, and navigation from main menu

--- app/src/main/java/com/example/lando/tetris/MainActivity.kt
+++ app/src/main/java/com/example/lando/tetris/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.lando.tetris
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnExit: Button = findViewById(R.id.btn_exit)
+        val btnReset: Button = findViewById(R.id.btn_reset_score)
+        val btnNewGame: Button = findViewById(R.id.btn_new_game)
+        btnExit.setOnClickListener(this::handleExitEvent)
+        btnNewGame.setOnClickListener(this::handleNewGameEvent)
+    }
+    fun handleExitEvent(view: View){
+        finish()
+    }
+    fun handleNewGameEvent(view: View){
+        startActivity(Intent(this, GameActivity::class.java))
     }
 }
